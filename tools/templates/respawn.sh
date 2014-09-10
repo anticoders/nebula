284aaaabeb35a436c4fc0ba675d9b08fe142af36
apendua <apendua@gmail.com>
2014-09-10 11:14:56 +0200

    Ignore error if the file does not exist

--- tools/templates/respawn.sh
+++ tools/templates/respawn.sh
@@ -1,5 +1,5 @@
 # use diff to check if it's necessary
 sudo service {{name}} stop
-sudo rm /etc/init/{{name}}.conf
+sudo rm -f /etc/init/{{name}}.conf
 sudo cp {{pathToAssets}}/upstart.conf /etc/init/{{name}}.conf
 sudo service {{name}} start
