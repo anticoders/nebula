63800ab09fecc9b79d76f85877a807d898c252df
apendua <apendua@gmail.com>
2014-09-19 14:19:03 +0200

    In build script allow meteor app in example subdirectory

--- tools/templates/build.sh
+++ tools/templates/build.sh
@@ -2,11 +2,30 @@
 cd {{pathToSource}}
 HASH=`git rev-parse --short {{sha}}`
 
+if [ ! -e .meteor ] && [ -e example/.meteor ];
+then
+  echo "using example directory"
+  cd example
+else
+  echo "it does not seem to be a meteor project"
+  exit 1
+fi
+
 if [ ! -e {{pathToBuilds}}/${HASH} ];
 then
   echo "creating meteor bundle"
+  
+  # TODO: alter the behavior for older releases (bundle is deprecated since 0.9.2)
+  #RELEASE=`cat .meteor/release | sed 's/METEOR@//'`;
+  #if [ $RELEASE \< "0.9.2" ];
+  #then
+  #fi
+
   mkdir -p {{pathToBuilds}}
-  mrt install && meteor bundle {{pathToBuilds}}/bundle.tar.gz
+
+  # TODO: only use meteorite if smart.json is present
+  mrt install
+  meteor bundle {{pathToBuilds}}/bundle.tar.gz
   (
     cd {{pathToBuilds}}
     echo "extracting tar"
